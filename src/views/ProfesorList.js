a50ed64229a6d7e9df620a9bfae222e977c8b88f
PalomaZamorano <paloma.zamorano@usach.cl>
2019-12-24 02:48:04 -0300

    Se conecta la vista con el backend

--- src/views/ProfesorList.js
+++ src/views/ProfesorList.js
@@ -8,6 +8,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import axios from 'axios';
 
 
@@ -20,6 +22,8 @@ class Profesors extends Component {
 
         super(props);
         this.state={
+          ready:false,
+          profesors:[],
           rowsPerPage: 15,  
           page:  0,
           useStyles : makeStyles({
@@ -31,63 +35,49 @@ class Profesors extends Component {
             },
           }),
 
-        rows : [
-            this.createData('India', 'IN', 1324171354, 3287263),
-            this.createData('China', 'CN', 1403500365, 9596961),
-            this.createData('Italy', 'IT', 60483973, 301340),
-            this.createData('United States', 'US', 327167434, 9833520),
-            this. createData('Canada', 'CA', 37602103, 9984670),
-            this.createData('Australia', 'AU', 25475400, 7692024),
-            this.createData('Germany', 'DE', 83019200, 357578),
-            this.createData('Ireland', 'IE', 4857000, 70273),
-            this.createData('Mexico', 'MX', 126577691, 1972550),
-            this.createData('Japan', 'JP', 126317000, 377973),
-            this.createData('France', 'FR', 67022000, 640679),
-            this.createData('United Kingdom', 'GB', 67545757, 242495),
-            this.createData('Russia', 'RU', 146793744, 17098246),
-            this.createData('Nigeria', 'NG', 200962417, 923768),
-            this.createData('Brazil', 'BR', 210147125, 8515767),
-          ],
+        
+        rows2:[] ,
 
-        columns : [
-            { id: 'name', label: 'Name', minWidth: 170 },
-            { id: 'code', label: 'ISO\u00a0Code', minWidth:50 },
-            {
-              id: 'population',
-              label: 'Population',
-              minWidth: 170,
-              align: 'right',
-              format: value => value.toLocaleString(),
-            },
-            {
-              id: 'size',
-              label: 'Size\u00a0(km\u00b2)',
-              minWidth: 170,
-              align: 'right',
-              format: value => value.toLocaleString(),
-            },
-            {
-              id: 'density',
-              label: 'Density',
-              minWidth: 170,
-              align: 'right',
-              format: value => value.toFixed(2),
-            },
-          ]
+        columns2: [
             
-          
-
+                { id: 'name', label: 'Name', minWidth: 300, align: 'left' },
+                { id: 'Perfil', label: 'Perfil', minWidth: 50 , align: 'right'},
+                {
+                  id: 'Resultados por curso',
+                  label: 'Resultados por curso',
+                  minWidth: 170,
+                  align: 'right',
+                }
+             ] 
             
         }
       }
 
 
+componentDidMount() {
 
-createData(name, code, population, size) {
-        const density = population / size;
-        return { name, code, population, size, density };
-      }  
- 
+        axios.get(`http://localhost:3000/profesors.json`)
+        .then(res => {
+          const profesors = res.data;
+        
+          if(profesors.length !== 0){
+            this.state.rows2.push(profesors.map((profesor,index) =>  
+            this.createData2(profesor.prof_nombre_corto)
+            )
+            );
+          }
+            const ready = true;  
+            this.setState({ ready });
+         
+        })
+} 
+
+
+createData2(name) {
+    return { name};
+  } 
+
+  
 handleChangePage = (event, page) => {
     this.setState({ page });
       };
@@ -100,59 +90,71 @@ handleChangeRowsPerPage = event => {
 render(){
 
     const { rowsPerPage, page } = this.state;
+    if(this.state.ready === false){
+        
+        return(
+          <div>
+             <CircularProgress size={150} color="secondary" />
+          </div>
     
-
-  return (
-    <div   style = {{height:620}} >
-    <Paper className={this.state.useStyles.root}>
-     <Grid container style = {{marginTop : 60}} >
-     <Grid item  xs={12} md={12} >
-        <Table stickyHeader aria-label="sticky table" size="small">
-          <TableHead>
-            <TableRow>
-              {this.state.columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {this.state.columns.map(column => {
-                    const value = row[column.id];
+        )
+    
+    }
+    else{
+       
+        return (
+            <div   style = {{height:620}} >
+            <Paper className={this.state.useStyles.root}>
+            <Grid container style = {{marginTop : 60}} >
+            <Grid item  xs={12} md={12} >
+                <Table stickyHeader aria-label="sticky table" size="small" style = {{width :640}}>
+                <TableHead>
+                    <TableRow>
+                    {this.state.columns2.map(column => (
+                        <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                        >
+                        {column.label}
+                        </TableCell>
+                    ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.state.rows2[0].slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
                     return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
+                        <TableRow hover role="checkbox" tabIndex={-1} key={row.id} >
+                        {this.state.columns2.map(column => {
+                            const value = row[column.id];
+                            return (
+                            <TableCell key={column.id} align={column.align}  style={{ fontSize:13 }}>
+                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                            </TableCell>
+                            );
+                        })}
+                        </TableRow>
                     );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      <TablePagination
-        rowsPerPageOptions={[]}
-        component="div"
-        count={this.state.rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={this.handleChangePage}
-        onChangeRowsPerPage={this.handleChangeRowsPerPage}
-      />
+                    })}
+                </TableBody>
+                </Table>
+             <TablePagination
+                rowsPerPageOptions={[]}
+                component="div"
+                count={this.state.rows2[0].length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={this.handleChangePage}
+                onChangeRowsPerPage={this.handleChangeRowsPerPage}
+             />
 
-        </Grid>
-        </Grid> 
-    </Paper>
-    </div>
+            </Grid>
+            </Grid> 
+            </Paper>
+            </div>
   );
+
+        } 
     }
 }
 
